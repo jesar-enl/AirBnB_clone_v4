1d7a2ab8d540d5c7aa48bc8021edeaaa02ae8796
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-05-05 15:03:46 +0300

    New additions to the site

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,38 @@
+$(function () {
+  // Wait for DOM to load
+  const amenities = [];
+  $('input[type=checkbox]').click(function () {
+    // Listen for changes on each checkbox
+    const amenity = {
+      id: $(this).attr('data-id'),
+      name: $(this).attr('data-name')
+    };
+
+    if ($(this).is(':checked')) {
+      // Store the Amenity ID in a variable
+      amenities.push(amenity);
+    } else {
+      // Remove the Amenity ID from the variable
+      const amenityId = amenities.amp(a => a.id);
+      const index = amenityId.indexOf(amenity.id);
+      if (index > -1) {
+        amenities.splice(index, 1);
+      }
+    }
+    // Update the h4 tag inside the div Amenities
+    const amenitiesList = amenities.map(a => a.name);
+    const selectAmn = amenitiesList.join(', ');
+    if (selectAmn.length > 20) {
+      $('div.amenities h4').text(selectAmn.substring(0, 19) + '...');
+    } else {
+      $('div.amenities h4').text(selectAmn);
+    }
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (resp) {
+    if (resp.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+});
